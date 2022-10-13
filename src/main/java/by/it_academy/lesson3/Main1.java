f717a353af218fbcf079d51626cf4f53269c0161
Alla35 <she10@mail.ru>
2022-10-13 18:38:03 +0300

    corrected

--- src/main/java/by/it_academy/lesson3/Main1.java
+++ src/main/java/by/it_academy/lesson3/Main1.java
@@ -3,20 +3,21 @@ package by.it_academy.lesson3;
 public class Main1 {
     public static void main(String[] args) {
 
-        int giftValue = 8000;
+        int giftValue = 2000;
         double tax;
 
         if (giftValue < 5000) {
             tax = 0;
 
-        } else if (giftValue >= 5000 && giftValue < 25000); {
+        } else if (giftValue >= 5000 && giftValue < 25000) {
             tax = (100 + (giftValue - 5000) * 0.08);
 
-        } if (giftValue > 25000) {
+        } else {
             tax = (1700 + (giftValue - 25000) * 0.1);
 
-        } else {
-            System.out.println("tax = " + tax);
         }
+        System.out.println("tax =" + tax);
+
     }
 }
+
