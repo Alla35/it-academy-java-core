a9d25ac7c47bb3b7982b4d5e1bbc9199c67c3ea6
Alla35 <she10@mail.ru>
2022-10-13 18:32:05 +0300

    corrected

--- src/main/java/by/it_academy/lesson3/Main2.java
+++ src/main/java/by/it_academy/lesson3/Main2.java
@@ -7,17 +7,22 @@ public class Main2 {
         switch (grade) {
             case 'A':
                 System.out.println("Distinction");
+                break;
             case 'B':
                 System.out.println("First class");
                 break;
             case 'C':
                 System.out.println("First class");
+                break;
             case 'D':
                 System.out.println("You have passed");
+                break;
             case 'F':
                 System.out.println("Fail.Try again");
+                break;
             default:
                 System.out.println("Invalid grade");
-    }
+                break;
+        }
     }
 }
