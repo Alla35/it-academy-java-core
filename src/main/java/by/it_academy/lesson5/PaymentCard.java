8eb0fb372a289b5f76aa70cc260d0deeec3bd30d
Maxim Tereshchenko <maksimt@backbase.com>
2022-10-24 18:00:46 +0200

    fixed

--- src/main/java/by/it_academy/lesson5/PaymentCard.java
+++ src/main/java/by/it_academy/lesson5/PaymentCard.java
@@ -2,42 +2,43 @@ package by.it_academy.lesson5;
 
 public class PaymentCard {
 
-    private static double balance;
+    private double balance;
 
     PaymentCard(double balance) {
-        this.balance = balance; }
-
-    @Override
-    public String toString() {
-        return "The card has a balance of X" + balance;
+        this.balance = balance;
     }
 
-    public static void eatAffordably() {
+    public static void main(String[] args) {
+        PaymentCard paymentCard = new PaymentCard(50);
+        paymentCard.eatAffordably();
+        paymentCard.eatHeartily();
+        paymentCard.addMoney(50);
+        System.out.println(paymentCard);
+    }
+
+    public void eatAffordably() {
         if (balance >= 2.6) {
             balance -= 2.6;
         }
     }
 
-    public static void eatHeartily() {
+    public void eatHeartily() {
         if (balance >= 4.6) {
             balance -= 4.6;
         }
     }
 
-    public static void addMoney(double amount) {
+    public void addMoney(double amount) {
         if (amount > 0) {
             balance += amount;
         }
     }
 
-    public static void main(String[] args) {
-
-        PaymentCard paymentCard = new PaymentCard(50);
-        PaymentCard.eatAffordably();
-        PaymentCard.eatHeartily();
-        PaymentCard.addMoney(50);
-        System.out.println(paymentCard);
+    @Override
+    public String toString() {
+        return "The card has a balance of " + balance;
     }
+
 }
 
 
--- src/main/java/by/it_academy/lesson5/Whistle.java
+++ src/main/java/by/it_academy/lesson5/Whistle.java
@@ -3,17 +3,18 @@ package by.it_academy.lesson5;
 public class Whistle {
 
     private final String sound;
-    public Whistle (final String sound) {
+
+    public Whistle(final String sound) {
         this.sound = sound;
+    }
+
+    public static void main(String[] args) {
+        Whistle whistle = new Whistle("do");
+        whistle.sound();
     }
 
     public void sound() {
         System.out.println(sound);
     }
 
-
-    public static void main(String[] args) {
-        Whistle whistle = new Whistle("do");
-        whistle.sound();
-    }
 }
