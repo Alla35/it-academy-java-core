218a4900524f149098c31f6e2f72322ce1e1c2f4
Alla35 <she10@mail.ru>
2022-11-07 23:38:04 +0300

    lesson8

---
+++ src/main/java/by/it_academy/lesson8/task1/Main.java
@@ -0,0 +1,23 @@
+package by.it_academy.lesson8.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Kaisa");
+        cat.purr();
+        cat.sleep();
+
+        Dog dog = new Dog("Bim");
+        dog.bark();
+        dog.sleep();
+
+        AnimalHouse<Cat> catHouse = new AnimalHouse<>();
+        catHouse.setAnimal(cat);
+        Cat cat1 = catHouse.getAnimal();
+
+        AnimalHouse<Dog> dogHouse = new AnimalHouse<>();
+        dogHouse.setAnimal(dog);
+
+    }
+
+}
---
+++ src/main/java/by/it_academy/lesson8/task2/Main.java
@@ -0,0 +1,8 @@
+package by.it_academy.lesson8.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Card someCard = new Card(Suit.DIAMOND, 12);
+        System.out.println(someCard);
+    }
+}
